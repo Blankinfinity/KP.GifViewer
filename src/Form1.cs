f7b0afa217ac4f82017b8d238461cf012a93be6a
Keith Pearce <keith@keith-pearce.co.uk>
2019-05-26 11:42:25 +0100

    Migrate from VSO

---
+++ src/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KP.GifViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                try
+                {
+                    pictureBox1.Image = Image.FromFile(file);                    
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+    }
+}
